6e966d6193fb0dd124e0e238ba73c89a365b6fe0
Aldrha Petit <aldrha@gmail.com>
2025-10-07 16:57:45 -0400

    quede en componentes de busqueda

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,19 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      {/* Header */}
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+    </>
+  );
 };
 
 export default SearchPage;
