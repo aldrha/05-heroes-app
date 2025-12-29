81f720ca1aaf37f639ac888b6950c0710f9efef6
Aldrha Petit <aldrha@gmail.com>
2025-12-29 16:50:11 -0400

    custom pagination 1

---
+++ src/components/custom/CustomPagination.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import type { PropsWithChildren } from 'react';
+import { MemoryRouter } from 'react-router';
+import { describe, expect, test, vi } from 'vitest';
+import { CustomPagination } from './CustomPagination';
+
+vi.mock('../ui/button', () => ({
+    Button: ({ children, ...props }: PropsWithChildren) => <button {...props}>{children}</button>,
+}));
+
+const renderWithRouter = (component: React.ReactElement) => {
+    return render(<MemoryRouter>{component}</MemoryRouter>);
+};
+
+describe('CustomPagination', () => {
+    test('should render component with default values', () => {
+        renderWithRouter(<CustomPagination totalPages={5} />);
+
+        expect(screen.getByText('Anterior')).toBeDefined();
+        expect(screen.getByText('Siguiente')).toBeDefined();
+
+        expect(screen.getByText('1')).toBeDefined();
+        expect(screen.getByText('2')).toBeDefined();
+        expect(screen.getByText('3')).toBeDefined();
+        expect(screen.getByText('4')).toBeDefined();
+        expect(screen.getByText('5')).toBeDefined();
+    });
+});
